eca1a980ad94bb193bf9d9d3e0f29a4bf2d285c0
wangden1012 <133735808+wangden1012@users.noreply.github.com>
2024-09-22 16:46:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,7 @@ from option_pricing import BlackScholesModel, MonteCarloPricing, BinomialTreeMod
 
 class OPTION_PRICING_MODEL(Enum):
     BLACK_SCHOLES = 'Black Scholes Model'
-    MONTE_CARLO = 'Monte Carlo Simulation'
-    BINOMIAL = 'Binomial Model'
-
+  
 @st.cache
 def get_historical_data(ticker):
     """Getting historical data for speified ticker and caching it with streamlit app."""
@@ -60,72 +58,3 @@ if pricing_method == OPTION_PRICING_MODEL.BLACK_SCHOLES.value:
         st.subheader(f'Call option price: {call_option_price}')
         st.subheader(f'Put option price: {put_option_price}')
 
-elif pricing_method == OPTION_PRICING_MODEL.MONTE_CARLO.value:
-    # Parameters for Monte Carlo simulation
-    ticker = st.text_input('Ticker symbol', 'AAPL')
-    strike_price = st.number_input('Strike price', 300)
-    risk_free_rate = st.slider('Risk-free rate (%)', 0, 100, 10)
-    sigma = st.slider('Sigma (%)', 0, 100, 20)
-    exercise_date = st.date_input('Exercise date', min_value=datetime.today() + timedelta(days=1), value=datetime.today() + timedelta(days=365))
-    number_of_simulations = st.slider('Number of simulations', 100, 100000, 10000)
-    num_of_movements = st.slider('Number of price movement simulations to be visualized ', 0, int(number_of_simulations/10), 100)
-
-    if st.button(f'Calculate option price for {ticker}'):
-        # Getting data for selected ticker
-        data = get_historical_data(ticker)
-        st.write(data.tail())
-        Ticker.plot_data(data, ticker, 'Adj Close')
-        st.pyplot()
-
-        # Formating simulation parameters
-        spot_price = Ticker.get_last_price(data, 'Adj Close') 
-        risk_free_rate = risk_free_rate / 100
-        sigma = sigma / 100
-        days_to_maturity = (exercise_date - datetime.now().date()).days
-
-        # ESimulating stock movements
-        MC = MonteCarloPricing(spot_price, strike_price, days_to_maturity, risk_free_rate, sigma, number_of_simulations)
-        MC.simulate_prices()
-
-        # Visualizing Monte Carlo Simulation
-        MC.plot_simulation_results(num_of_movements)
-        st.pyplot()
-
-        # Calculating call/put option price
-        call_option_price = MC.calculate_option_price('Call Option')
-        put_option_price = MC.calculate_option_price('Put Option')
-
-        # Displaying call/put option price
-        st.subheader(f'Call option price: {call_option_price}')
-        st.subheader(f'Put option price: {put_option_price}')
-
-elif pricing_method == OPTION_PRICING_MODEL.BINOMIAL.value:
-    # Parameters for Binomial-Tree model
-    ticker = st.text_input('Ticker symbol', 'AAPL')
-    strike_price = st.number_input('Strike price', 300)
-    risk_free_rate = st.slider('Risk-free rate (%)', 0, 100, 10)
-    sigma = st.slider('Sigma (%)', 0, 100, 20)
-    exercise_date = st.date_input('Exercise date', min_value=datetime.today() + timedelta(days=1), value=datetime.today() + timedelta(days=365))
-    number_of_time_steps = st.slider('Number of time steps', 5000, 100000, 15000)
-
-    if st.button(f'Calculate option price for {ticker}'):
-         # Getting data for selected ticker
-        data = get_historical_data(ticker)
-        st.write(data.tail())
-        Ticker.plot_data(data, ticker, 'Adj Close')
-        st.pyplot()
-
-        # Formating simulation parameters
-        spot_price = Ticker.get_last_price(data, 'Adj Close') 
-        risk_free_rate = risk_free_rate / 100
-        sigma = sigma / 100
-        days_to_maturity = (exercise_date - datetime.now().date()).days
-
-        # Calculating option price
-        BOPM = BinomialTreeModel(spot_price, strike_price, days_to_maturity, risk_free_rate, sigma, number_of_time_steps)
-        call_option_price = BOPM.calculate_option_price('Call Option')
-        put_option_price = BOPM.calculate_option_price('Put Option')
-
-        # Displaying call/put option price
-        st.subheader(f'Call option price: {call_option_price}')
-        st.subheader(f'Put option price: {put_option_price}')
